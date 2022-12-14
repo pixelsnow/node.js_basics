f5fb323334ab0edcac41317f52c2c6c9d82a89b7
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-12-14 03:39:56 +0200

    add complete storageLayer

--- Vagapova_Valeriia_game_project/storage/storageLayer.js
+++ Vagapova_Valeriia_game_project/storage/storageLayer.js
@@ -10,6 +10,41 @@ const { adapt } = require(adapterFile);
 
 const storageFilePath = path.join(__dirname, storageFile);
 
-const getAllFromStorage = async () => readStorage(storageFile);
+const getAllFromStorage = async () => readStorage(storageFilePath);
 
-getAllFromStorage().then(console.log).catch(console.log);
+const getOneFromStorage = async (keyValue) => {
+  const allData = await readStorage(storageFilePath);
+  return allData.find((item) => item[key] == keyValue) || null;
+};
+
+const insertIntoStorage = async (newObject) => {
+  const allData = await readStorage(storageFilePath);
+  allData.push(newObject);
+  return await writeStorage(storageFilePath, allData);
+};
+
+const updateStorage = async (updatedObject) => {
+  const allData = await readStorage(storageFilePath);
+  const object = allData.find((item) => item[key] == updatedObject[key]);
+  if (!object) return false;
+  else {
+    Object.assign(object, adapt(updatedObject));
+    return await writeStorage(storageFilePath, allData);
+  }
+};
+
+const removeFromStorage = async (keyValue) => {
+  const allData = await readStorage(storageFilePath);
+  const objectIndex = allData.findIndex((item) => item[key] == keyValue);
+  if (objectIndex < 0) return false;
+  allData.splice(i, 1);
+  return await writeStorage(storageFilePath, allData);
+};
+
+module.exports = {
+  getAllFromStorage,
+  getOneFromStorage,
+  insertIntoStorage,
+  updateStorage,
+  removeFromStorage,
+};
